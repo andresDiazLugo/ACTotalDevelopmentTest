8c4e83bdc292c9a7febf58da121674d833cabf80
andresDiazLugo <andresd.ad5@gmail.com>
2025-02-10 19:16:37 -0300

    fix: chage file config

--- includes/config.php
+++ includes/config.php
@@ -9,11 +9,11 @@ if (getenv('ENV_MOD') === 'dev') { // Si no hay variables en el sistema, cargar 
 }
 
 // Usar variables de entorno con fallback
-$db_host = getenv('DB_HOST_PROD') ?: ($_ENV['DB_HOST_PROD'] ?? 'NO DEFINIDO');
-$db_port = getenv('DB_PORT_PROD') ?: ($_ENV['DB_PORT_PROD'] ?? 3306);
-$db_user = getenv('DB_USER_PROD') ?: ($_ENV['DB_USER_PROD'] ?? 'NO DEFINIDO');
-$db_password = getenv('DB_PASSWORD_PROD') ?: ($_ENV['DB_PASSWORD_PROD'] ?? '');
-$db_name = getenv('DB_NAME_PROD') ?: ($_ENV['DB_NAME_PROD'] ?? 'NO DEFINIDO');
+$db_host = $_SERVER['DB_HOST_PROD'] ?? $_ENV['DB_HOST_DEV'] ?? 'NO DEFINIDO';
+$db_port = $_SERVER['DB_PORT_PROD'] ?? $_ENV['DB_PORT_DEV'] ?? 'NO DEFINIDO';
+$db_user = $_SERVER['DB_USER_PROD'] ?? $_ENV['DB_USER_DEV'] ?? 'NO DEFINIDO';
+$db_password = $_SERVER['DB_PASSWORD_PROD'] ?? $_ENV['DB_PASSWORD_DEV'] ?? 'NO DEFINIDO';
+$db_name = $_SERVER['DB_NAME_PROD'] ?? $_ENV['DB_NAME_DEV'] ?? 'NO DEFINIDO';
 // Retornar configuración como array
 return [
     'db_host' => $db_host,
