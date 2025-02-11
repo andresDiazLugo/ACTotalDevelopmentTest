ea922f138c142ce80c678f177b2203c900d040de
andresDiazLugo <andresd.ad5@gmail.com>
2025-02-10 21:49:50 -0300

    fix: test database production

--- includes/config.php
+++ includes/config.php
@@ -12,7 +12,7 @@ $db_host = $modeENV === 'prod' ? "free.clusters.zeabur.com" : $_ENV['DB_HOST_DEV
 $db_port = $modeENV === 'prod'  ?  32461 : $_ENV['DB_PORT_DEV'];
 $db_user =  $modeENV === 'prod' ? "root": $_ENV['DB_USER_DEV'];
 $db_password =  $modeENV === 'prod' ? "9pXrZD4257GgQbP3S0EM6o8Bcvu1whfO"  : $_ENV['DB_PASSWORD_DEV'];
-$db_name =  $modeENV === 'prod' ? "zeabur" : $_ENV['DB_NAME_DEV'];
+$db_name =  $modeENV === 'prod' ? "actotaltest" : $_ENV['DB_NAME_DEV'];
 return [
     'db_host' => $db_host,
     'db_port' => $db_port,
