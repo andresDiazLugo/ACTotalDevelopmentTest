b61896f20d0854fa21bae9ab26e5b921f198b6a8
andresDiazLugo <andresd.ad5@gmail.com>
2025-02-10 22:00:52 -0300

    fix: test database production

--- includes/config.php
+++ includes/config.php
@@ -8,11 +8,11 @@ $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->safeLoad();
 
 // Usar variables de entorno con fallback
-$db_host = $modeENV === 'prod' ? "free.clusters.zeabur.com" : $_ENV['DB_HOST_DEV'];
-$db_port = $modeENV === 'prod'  ?  32461 : $_ENV['DB_PORT_DEV'];
-$db_user =  $modeENV === 'prod' ? "root": $_ENV['DB_USER_DEV'];
-$db_password =  $modeENV === 'prod' ? "9pXrZD4257GgQbP3S0EM6o8Bcvu1whfO"  : $_ENV['DB_PASSWORD_DEV'];
-$db_name =  $modeENV === 'prod' ? "actotaltest" : $_ENV['DB_NAME_DEV'];
+$db_host = $modeENV === 'prod' ? $_SERVER['DB_HOST_PROD'] : $_ENV['DB_HOST_DEV'];
+$db_port = $modeENV === 'prod' ?  $_SERVER['DB_PORT_PROD'] : $_ENV['DB_PORT_DEV'];
+$db_user =  $modeENV === 'prod' ? $_SERVER['DB_USER_PROD'] : $_ENV['DB_USER_DEV'];
+$db_password =  $modeENV === 'prod' ? $_SERVER['DB_PASSWORD_PROD'] : $_ENV['DB_PASSWORD_DEV'];
+$db_name =  $modeENV === 'prod' ? $_SERVER['DB_NAME_PROD'] : $_ENV['DB_NAME_DEV'];
 return [
     'db_host' => $db_host,
     'db_port' => $db_port,
